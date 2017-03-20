f247c1afb53b3a4f2de77cb779023174c4364dc8
Marco Perone <m.perone@mvlabs.it>
2017-03-20 08:44:11 +0100

    rename check return types

--- src/Command/CheckTypes.php
+++ src/Command/CheckTypes.php
@@ -10,7 +10,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-final class CheckReturnTypes extends Command
+final class CheckTypes extends Command
 {
     protected function configure()
     {
