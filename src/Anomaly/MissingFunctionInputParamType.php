80b940a66a376f2234d60c111bf70f2fb36cf9a5
Marco Perone <m.perone@mvlabs.it>
2017-03-22 09:45:09 +0100

    missing input param type anomalies

---
+++ src/Anomaly/MissingFunctionInputParamType.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Marcosh\PhpReturnTypeChecker\Anomaly;
+
+use Roave\BetterReflection\Reflection\ReflectionParameter;
+
+final class MissingFunctionInputParamType implements Anomaly
+{
+    /**
+     * @var ReflectionParameter
+     */
+    private $parameter;
+
+    private function __construct(ReflectionParameter $parameter)
+    {
+        $this->parameter = $parameter;
+    }
+
+    public static function param(ReflectionParameter $parameter): \Iterator
+    {
+        try {
+            $docBlockTypes = $parameter->getDocBlockTypes();
+        } catch (\InvalidArgumentException $e) {
+            // we need this here to prevent reflection-bocblock errors on @see invalid Fqsen
+        }
+
+        if (null === $parameter->getTypeHint() && empty($docBlockTypes)) {
+            yield new self($parameter);
+        }
+    }
+
+    public function message(): string
+    {
+        return sprintf(
+            'Parameter <info>%s</info> of function <info>%s</info> defined in <comment>%s</comment> ' .
+            'does not have a type hint.',
+            $this->parameter->getName(),
+            $this->parameter->getDeclaringFunction()->getName(),
+            $this->parameter->getDeclaringFunction()->getFileName()
+        );
+    }
+}
---
+++ src/Anomaly/MissingMethodInputParamType.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Marcosh\PhpReturnTypeChecker\Anomaly;
+
+use Roave\BetterReflection\Reflection\ReflectionParameter;
+
+final class MissingMethodInputParamType
+{
+    /**
+     * @var ReflectionParameter
+     */
+    private $parameter;
+
+    private function __construct(ReflectionParameter $parameter)
+    {
+        $this->parameter = $parameter;
+    }
+
+    public static function param(ReflectionParameter $parameter): \Iterator
+    {
+        try {
+            $docBlockTypes = $parameter->getDocBlockTypes();
+        } catch (\InvalidArgumentException $e) {
+            // we need this here to prevent reflection-bocblock errors on @see invalid Fqsen
+        }
+
+        if (null === $parameter->getTypeHint() && empty($docBlockTypes)) {
+            yield new self($parameter);
+        }
+    }
+
+    public function message(): string
+    {
+        return sprintf(
+            'Parameter <info>%s</info> of method <info>%s</info> of the class <info>%s</info> ' .
+            'defined in <comment>%s</comment> does not have a type hint.',
+            $this->parameter->getName(),
+            $this->parameter->getDeclaringFunction()->getName(),
+            $this->parameter->getDeclaringFunction()->getDeclaringClass()->getName(),
+            $this->parameter->getDeclaringFunction()->getFileName()
+        );
+    }
+}
