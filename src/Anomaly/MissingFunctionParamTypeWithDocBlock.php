2237de0c90a6a81555ef72d743456b1a067a8d03
Marco Perone <m.perone@mvlabs.it>
2017-03-24 07:54:13 +0100

    missing function parameter type with doc block anomaly

---
+++ src/Anomaly/MissingFunctionParamTypeWithDocBlock.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\PhpReturnTypeChecker\Anomaly;
+
+use Roave\BetterReflection\Reflection\ReflectionParameter;
+
+final class MissingFunctionParamTypeWithDocBlock
+{
+    /**
+     * @var ReflectionParameter
+     */
+    private $parameter;
+
+    public function __construct(ReflectionParameter $parameter)
+    {
+        $this->parameter = $parameter;
+    }
+
+    public static function param(ReflectionParameter $parameter)
+    {
+        try {
+            $docBlockTypes = $parameter->getDocBlockTypes();
+        } catch (\InvalidArgumentException $e) {
+            // we need this here to prevent reflection-bocblock errors on @see invalid Fqsen
+        }
+
+        if (null === $parameter->getTypeHint() && !empty($docBlockTypes)) {
+            yield new self($parameter);
+        }
+    }
+
+    public function message(): string
+    {
+        try {
+            $docBlockTypes = $this->parameter->getDocBlockTypes();
+        } catch (\InvalidArgumentException $e) {
+            // we need this here to prevent reflection-bocblock errors on @see invalid Fqsen
+        }
+
+        return sprintf(
+            'Parameter <info>%s</info> of function <info>%s</info> defined in <comment>%s</comment> ' .
+            'does not have a type hint but has a boc block type of <info>%s</info>',
+            $this->parameter->getName(),
+            $this->parameter->getDeclaringFunction()->getName(),
+            $this->parameter->getDeclaringFunction()->getFileName(),
+            implode($docBlockTypes)
+        );
+    }
+}
--- src/TypeHint/FunctionParamTypeHint.php
+++ src/TypeHint/FunctionParamTypeHint.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Marcosh\PhpReturnTypeChecker\TypeHint;
 
 use Marcosh\PhpReturnTypeChecker\Anomaly\MissingFunctionInputParamType;
+use Marcosh\PhpReturnTypeChecker\Anomaly\MissingFunctionParamTypeWithDocBlock;
 use Roave\BetterReflection\Reflection\ReflectionParameter;
 
 final class FunctionParamTypeHint
@@ -12,5 +13,6 @@ final class FunctionParamTypeHint
     public static function param(ReflectionParameter $parameter): \Iterator
     {
         yield from MissingFunctionInputParamType::param($parameter);
+        yield from MissingFunctionParamTypeWithDocBlock::param($parameter);
     }
 }
