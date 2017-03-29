bc3ec3c05202a6b314fa35b0e8479ea6dbfdb3c8
Marco Perone <m.perone@mvlabs.it>
2017-03-29 08:19:16 +0200

    test missing method param type with doc block anomaly

---
+++ test/Anomaly/MissingMethodParamTypeWithDocBlockTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\PhpTypeCheckerTest\Anomaly;
+
+use Marcosh\PhpTypeChecker\Anomaly\MissingMethodParamTypeWithDocBlock;
+use PHPUnit\Framework\TestCase;
+use Roave\BetterReflection\Reflector\ClassReflector;
+use Roave\BetterReflection\SourceLocator\Type\StringSourceLocator;
+
+final class MissingMethodParamTypeWithDocBlockTest extends TestCase
+{
+    public function testCorrectMessage()
+    {
+        $php = '<?php
+            class Foo {
+                /** @param int $x */
+                function foo($x) {}
+            }
+        ';
+
+        $reflector = new ClassReflector(new StringSourceLocator($php));
+        $class = $reflector->reflect('Foo');
+
+        $method = $class->getMethod('foo');
+
+        $parameter = $method->getParameter('x');
+
+        foreach(MissingMethodParamTypeWithDocBlock::param($parameter) as $anomaly) {
+            self::assertSame(
+                'Parameter <info>x</info> of method <info>foo</info> of the class <info>Foo</info> ' .
+                'defined in <comment></comment> does not have a type hint but has a boc block type of ' .
+                '<info>int</info>.',
+                $anomaly->message()
+            );
+        }
+    }
+}
