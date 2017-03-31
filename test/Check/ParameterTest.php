cacd93ecc67a1b4aaee486cb353b999a4428835a
Marco Perone <m.perone@mvlabs.it>
2017-03-31 08:31:51 +0200

    add parameter check tests

---
+++ test/Check/ParameterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Marcosh\PhpTypeCheckerTest\Check;
+
+use Marcosh\PhpTypeChecker\Check\Parameter;
+use PHPUnit\Framework\TestCase;
+use Roave\BetterReflection\Reflection\ReflectionParameter;
+
+final class ParameterTest extends TestCase
+{
+    public function parameterDataProvider()
+    {
+        return [
+            [ReflectionParameter::createFromClosure(function ($x) {}, 'x'), true, false, false],
+            [ReflectionParameter::createFromClosure(function (int $x) {}, 'x'), false, false, false],
+            [ReflectionParameter::createFromClosure(/** @param int $x */function (int $x) {}, 'x'), false, false, false],
+            [ReflectionParameter::createFromClosure(/** @param int $x */function ($x) {}, 'x'), false, true, false],
+            [ReflectionParameter::createFromClosure(/** @param int $x */function (string $x) {}, 'x'), false, false, true]
+        ];
+    }
+
+    /**
+     * @dataProvider parameterDataProvider
+     */
+    public function testParameter(
+        ReflectionParameter $parameter,
+        bool $typeMissing,
+        bool $typeMissingWithDocBlock,
+        bool $typeDoesNotCoincideWithDocBlock
+    ) {
+        $check = new Parameter($parameter);
+
+        self::assertSame($typeMissing, $check->typeIsMissing());
+        self::assertSame($typeMissingWithDocBlock, $check->typeIsMissingWithDocBlock());
+        self::assertSame($typeDoesNotCoincideWithDocBlock, $check->typeDoesNotCoincideWithDocBlock());
+    }
+}
