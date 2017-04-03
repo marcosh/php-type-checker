715904914ceae68024c485bc23d4a3affb2ddfdb
Marco Perone <m.perone@mvlabs.it>
2017-04-03 08:09:02 +0200

    array parameter

--- src/Check/Parameter.php
+++ src/Check/Parameter.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Marcosh\PhpTypeChecker\Check;
 
 use phpDocumentor\Reflection\Type;
+use phpDocumentor\Reflection\Types\Array_;
 use phpDocumentor\Reflection\Types\Mixed;
 use Roave\BetterReflection\Reflection\ReflectionParameter;
 
@@ -55,6 +56,7 @@ final class Parameter
 
         return !empty($docBlockTypes) &&
             $this->parameter->getTypeHint() instanceof Type &&
-            !in_array($this->parameter->getTypeHint(), $docBlockTypes, false);
+            !in_array($this->parameter->getTypeHint(), $docBlockTypes, false) &&
+            !($this->parameter->getTypeHint() instanceof Array_ && $docBlockTypes[0] instanceof Array_);
     }
 }
